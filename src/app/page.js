d22b56ac3cc188fd2a561c053b5a9944e0b1f315
Alexis Dubshek <dubshek@hotmail.com>
2023-10-31 20:22:13 -0600

    Primer push

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import Data from './data2'
+
+export default function Home() {
+  return (
+
+    <div
+    className='w-[80%] mx-auto'
+    >
+    <Data categ={"React"}/>
+    <Data categ={"Web"}/>
+    <Data categ={"Networking"}/>
+
+    <Data categ={"HTML"}/>
+
+
+    
+    </div>
+  )
+}
