99d16eb0c7124ef52663303fcb5ce9e767a068ed
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2023-11-04 16:59:22 -0600

    feat(navbar): Implementación de búsqueda de libros de todas las categorías

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,16 @@
 import Data from './data2'
+import SearchButton from '@/components/SearchButton'
 
 export default function Home() {
   return (
 
     <div
-    className='w-[80%] mx-auto'
+      className='w-[80%] mx-auto'
     >
-    <Data categ={"React"}/>
-    <Data categ={"Web"}/>
-    <Data categ={"Networking"}/>
-
-    <Data categ={"HTML"}/>
-
-
-    
+      <Data categ={"React"} />
+      <Data categ={"Web"} />
+      <Data categ={"Networking"} />
+      <Data categ={"HTML"} />
     </div>
   )
 }
